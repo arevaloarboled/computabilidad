03f2273b149361035f23a6b67f678d64952c0c45
arevaloarboled <arevaloarboled@gmail.com>
2014-12-01 12:27:39 -0500

    version final y sustentada

--- computa.cpp
+++ computa.cpp
@@ -112,7 +112,7 @@ public:
 
 	void crear(){
 		int x,y,z;
-		cout<<"numero de aristas";
+		cout<<"numero de aristas: ";
 		cin>>y;
 		char s;
 		for (int i = 0; i < y; ++i)
@@ -463,6 +463,10 @@ public:
 				}
 			}
 			// cout<<"9"<<endl;
+			// for (int j = 0; j < alfabeto.size(); ++j)
+			// {
+			// 	cout<<alfabeto[j]<<endl;
+			// }
 			for (int j = 0; j < alfabeto.size(); ++j)
 			{
 				// cout<<"6"<<endl;
@@ -733,44 +737,328 @@ public:
 		 system("dot -Tpng img.dot -o img.png");
 	}
 
+	void cargar(string name){
+		string s;
+		ifstream inp;
+		inp.open(name.c_str());
+		inp>>s;
+		grafo gg(1);
+		for (int i = 0; i < s.size(); ++i)
+		{
+			// cout<<s[i]<<endl;
+			gg.alfabeto.push_back(s[i]);
+		}
+		// cout<<alfabeto.size()<<endl;
+		// int i=0;
+		// while(s[i]!=' '){
+		// 	gg.alfabeto.push_back(s[i]);
+		// 	i++;
+		// }
+		int x,y,a,b;
+		char c;
+		inp>>s;
+		x=atoi(s.c_str());
+		grafo ggg(x);
+		ggg.alfabeto=gg.alfabeto;
+		gg=ggg;
+		inp>>s;
+		y=atoi(s.c_str());
+		for (int i = 0; i < y; ++i)
+		{
+			inp>>s;
+			a=atoi(s.c_str());
+			inp>>s;
+			b=atoi(s.c_str());
+			inp>>c;
+			gg.addarista(a,b,c);
+		}
+		inp>>s;
+		y=atoi(s.c_str());
+		for (int i = 0; i < y; ++i)
+		{
+			inp>>s;
+			a=atoi(s.c_str());
+			gg.inicial.push_back(a);
+		}
+		inp>>s;
+		y=atoi(s.c_str());
+		for (int i = 0; i < y; ++i)
+		{
+			inp>>s;
+			a=atoi(s.c_str());
+			gg.final.push_back(a);
+		}
+
+		inp.close();
+		nodos=gg.nodos;
+		aristas=gg.aristas;
+		inicial=gg.inicial;
+		final=gg.final;
+		alfabeto=gg.alfabeto;
+
+
+		// char s[100000];
+		// inp.open(name.c_str());
+		// // inp>>s;
+		// // cout<<s<<endl;
+		// inp.getline(s,100000);
+		// int i=0;
+		// grafo gg(0);
+		// // cout<<s[i]<<endl;
+		// // vector <vector<arista> > aristass;
+		// // vector <int> iniciall;
+		// // vector <int> finall;
+		// // vector<char> alfabetoo;
+		// // vector <node> nodoss;
+		// while(s[i]!=' '){
+		// 	// cout<<s[i]<<endl;
+		// 	gg.alfabeto.push_back(s[i]);
+		// 	i++;
+		// }
+		// // i=0;
+		// inp>>s;
+		// i++;
+		// string tmp;
+		// while(s[i]!=' '){
+		// 	// cout<<s[i]<<endl;
+		// 	tmp+=s[i];
+		// 	// inp>>s;
+		// 	i++;
+		// }
+		// i++;
+		// // cout<<s<<endl;
+		// // cout<<s[i]<<endl;
+		// gg.nodos.resize(atoi(tmp.c_str()));
+		// gg.aristas.resize(atoi(tmp.c_str()));
+		// for (int i = 0; i < gg.aristas.size(); ++i)
+		// {
+		// 	gg.aristas[i].resize(atoi(tmp.c_str()));
+		// }
+		// tmp.clear();
+		// while(s[i]!=' '){
+		// 	// cout<<s[i]<<endl;
+		// 	tmp+=s[i];
+		// 	// inp>>s;
+		// 	i++;
+		// }
+		// i++;
+		// // cout<<s[i]<<endl;
+		// int n=atoi(tmp.c_str());
+		// for (int j = 0; j < n; ++j)
+		// {
+		// 	int x,y,z;
+		// 	tmp.clear();
+		// 	while(s[i]!=' '){
+		// 		// cout<<s[i]<<endl;
+		// 		tmp+=s[i];
+		// 		// inp>>s;
+		// 		i++;
+		// 	}	
+		// 	x=atoi(tmp.c_str());
+		// 	i++;
+		// 	// cout<<s[i]<<endl;
+		// 	tmp.clear();
+		// 	while(s[i]!=' '){
+		// 		// cout<<s[i]<<endl;
+		// 		tmp+=s[i];
+		// 		// inp>>s;
+		// 		i++;
+		// 	}
+		// 	y=atoi(tmp.c_str());
+		// 	i++;
+		// 	// cout<<s[i]<<endl;
+		// 	tmp.clear();
+		// 	gg.addarista(x,y,s[i]);
+		// 	i++;
+		// }
+		// while(s[i]!=' '){
+		// 	// cout<<s[i]<<endl;
+		// 	tmp+=s[i];
+		// 	// inp>>s;
+		// 	i++;
+		// }
+		// i++;
+		// // cout<<s[i]<<endl;
+		// n=atoi(tmp.c_str());
+		// tmp.clear();
+		// for (int i = 0; i < n; ++i)
+		// {
+		// 	while(s[i]!=' '){
+		// 		// cout<<s[i]<<endl;
+		// 		tmp+=s[i];
+		// 		// inp>>s;
+		// 		i++;
+		// 	}	
+		// 	i++;
+		// 	// cout<<s[i]<<endl;
+		// 	gg.inicial.push_back(atoi(tmp.c_str()));
+		// 	tmp.clear();
+		// }
+		// while(s[i]!=' '){
+		// 	// cout<<s[i]<<endl;
+		// 	tmp+=s[i];
+		// 	// inp>>s;
+		// 	i++;
+		// }
+		// i++;
+		// // cout<<s[i]<<endl;
+		// n=atoi(tmp.c_str());
+		// tmp.clear();
+		// for (int i = 0; i < n; ++i)
+		// {
+		// 	while(s[i]!=' '){
+		// 		// cout<<s[i]<<endl;
+		// 		tmp+=s[i];
+		// 		// inp>>s;
+		// 		i++;
+		// 	}	
+		// 	i++;
+		// 	// cout<<s[i]<<endl;
+		// 	gg.final.push_back(atoi(tmp.c_str()));
+		// 	tmp.clear();
+		// }
+		// inp.close();
+		// nodos=gg.nodos;
+		// aristas=gg.aristas;
+		// inicial=gg.inicial;
+		// final=gg.final;
+		// alfabeto=gg.alfabeto;
+		// // while(!inp.eof()){
+		// // 	inp >> s;
+		// // }
+	}
+
+	void grabar(string name){
+		string expor;
+		for (int i = 0; i < alfabeto.size(); ++i)
+		{
+			expor+=alfabeto[i];
+		}
+		expor+=" ";
+		expor+=to_string(nodos.size());
+		expor+=" ";
+		string tmp;
+		int n=0;
+		for (int i = 0; i < nodos.size(); ++i)
+		{
+			for (int j = 0; j < nodos.size(); ++j)
+			{
+				for (int k = 0; k < aristas[i][j].valor.size(); ++k)
+				{
+					if (aristas[i][j].valor[k]!=' ')
+					{
+						n++;
+						tmp+=to_string(i);
+						tmp+=" ";
+						tmp+=to_string(j);
+						tmp+=" ";
+						tmp+=aristas[i][j].valor[k];
+						tmp+=" ";
+					}
+				}
+			}
+		}
+	expor+=to_string(n);
+	expor+=" ";
+	expor+=tmp;
+	expor+=to_string(inicial.size());
+	expor+=" ";
+	for (int i = 0; i < inicial.size(); ++i)
+	{
+		expor+=to_string(inicial[i]);
+		expor+=" ";
+	}
+	expor+=to_string(final.size());
+	expor+=" ";
+	for (int i = 0; i < final.size(); ++i)
+	{
+		expor+=to_string(final[i]);
+		expor+=" ";
+	}
+	ofstream output;
+	name+=".puj";
+	output.open(name.c_str());
+	output << expor;
+	output.close();
+	}
 };
 
 grafo minimizacion(grafo g){
 	// cout<<"asdf"<<endl;
 	g=g.reverso();
-	g.print();
-	cout<<"step 1"<<endl;
+	// g.print();
+	// cout<<"step 1"<<endl;
 	g=g.determinista();
-	g.print();
-	cout<<"step 2"<<endl;
+	// g.print();
+	// cout<<"step 2"<<endl;
 	g=g.reverso();
-	g.print();
-	cout<<"step 3"<<endl;
+	// g.print();
+	// cout<<"step 3"<<endl;
 	g=g.determinista();
 	return g;
 }
 
-	// grafo union(grafo g){
-	// 	grafo gg(2+nodos.siz()+g.nodos.size());
-	// 	gg.addarista(0,1,'#');
-	// 	int n=1;
-	// 	for (int i = 0; i < nodos.size(); ++i)
-	// 	{
-	// 		for (int j = 0; j < nodos.size(); ++j)
-	// 		{
-	// 			gg.aristas[i+n][j+n]=aristas[i][j];
-	// 		}
-	// 	}
-	// 	n=n+nodos.size();
-	// 	for (int i = 0; i < nodos.size(); ++i)
-	// 	{
-	// 		for (int j = 0; j < nodos.size(); ++j)
-	// 		{
-	// 			gg.aristas[i+n][j+n]=g.aristas[i][j];
-	// 		}
-	// 	}
-	// 	return gg;
+
+grafo nion(grafo g , grafo go ){
+	grafo gg(2+g.nodos.size()+go.nodos.size());
+	gg.addarista(0,1,'#');
+	gg.inicial.push_back(0);
+	int n=1;
+	for (int i = 0; i < g.nodos.size(); ++i)
+	{
+		for (int j = 0; j < g.nodos.size(); ++j)
+		{
+			gg.aristas[i+n][j+n]=g.aristas[i][j];
+			// if (find(g.inicial.begin(), g.inicial.end(),i)!=g.inicial.end())
+			// {
+			// 	gg.inicial.push_back(i+n);
+			// }
+			if (find(g.final.begin(), g.final.end(),i)!=g.final.end())
+			{
+				gg.final.push_back(i+n);
+			}
+		}
+	}
+	n=n+g.nodos.size();
+	gg.addarista(0,n+1,'#');
+	n++;
+	for (int i = 0; i < go.nodos.size(); ++i)
+	{
+		for (int j = 0; j < go.nodos.size(); ++j)
+		{
+			gg.aristas[i+n][j+n]=go.aristas[i][j];
+			// if (find(go.inicial.begin(), go.inicial.end(),i)!=go.inicial.end())
+			// {
+			// 	gg.inicial.push_back(i+n);
+			// }
+			if (find(go.final.begin(), go.final.end(),i)!=go.final.end())
+			{
+				gg.final.push_back(i+n);
+			}
+		}
+	}
+	gg.print();
+	// vector <char> miow;
+	// for (int i = 0; i < g.alfabeto.size(); ++i)
+	// {
+	// 	// cout<<"sdkhfdsahfsda"<<endl;
+	// 	// miow.push_back(g.alfabeto[i]);
+	// 	gg.alfabeto.push_back(g.alfabeto[i]);
 	// }
+	gg.alfabeto=g.alfabeto;
+	for (int i = 0; i < go.alfabeto.size(); ++i)
+	{
+		if (find(gg.alfabeto.begin(), gg.alfabeto.end(),go.alfabeto[i])==gg.alfabeto.end())
+		{
+			// miow.push_back(go.alfabeto[i]);
+			// cout<<"sdksandjfkdshajkfshlfs"<<endl;
+			gg.alfabeto.push_back(go.alfabeto[i]);
+		}
+	}
+
+	// gg.alfabeto=miow;
+	return gg;
+}
 
 // class automata
 // {
@@ -788,8 +1076,8 @@ int main(int argc, char const *argv[])
 	while(1){
 		while(1){
 			cout<<"¿que desea hacer?"<<endl;
-			cout<<"1.crear automata"<<endl;
-			cout<<"2.cargar automata"<<endl;
+			cout<<"1.crear automata: "<<endl;
+			cout<<"2.cargar automata: "<<endl;
 			cin>>a;
 			if (a==1)
 			{
@@ -802,24 +1090,124 @@ int main(int argc, char const *argv[])
 			}
 			if (a==2)
 			{
-				cout<<"cual desea cargar?"<<endl;
-				system("ls >> tmp.txt");
-				// ofstream output;
-				//  output.open("tmp.txt");
-				FILE *output;
-				output=fopen("tmp.txt","r");
-				 string line;
-				 vector <string> vs;
-				 // while(getline(output,line)){
-				 getline(output,line);
-				 	if (find(line.begin(), line.end(),".puj")!=line.end())
-				 	{
-				 		vs.push_back(line);
-				 		cout<<vs.size()<<") "<<line<<endl;
-				 	}
-				 // }
-				 cin>>c;
-				 cout<<vs[c]<<endl;
+				cout<<"cual desea cargar? "<<endl;
+				// system("ls >> tmp.txt");
+				// ifstream ip;
+				//  ip.open("tmp.txt");
+				//  string line;
+				//  vector <string> vs;
+				//  // while(getline(output,line)){
+				//  ip>>line;
+				//  	if (substr)
+				//  	{
+				//  		vs.push_back(line);
+				//  		cout<<vs.size()<<") "<<line<<endl;
+				//  	}
+				//  // }
+				//  cin>>c;
+				//  cout<<vs[c]<<endl;
+				string ss;
+				cin>>ss;
+				g.cargar(ss);
+				for (int i = 0; i < g.alfabeto.size(); ++i)
+				{
+					cout<<g.alfabeto[i]<<endl;
+				}
+				// g.expor();
+				// system("display img.png");
+				break;
+			}
+		}
+		while(1){
+			int n;
+			cout<<"que desea hacer?"<<endl;
+			cout<<"1) grabar"<<endl;
+			cout<<"2) determinismo"<<endl;
+			cout<<"3) minimizacion"<<endl;
+			cout<<"4) ver"<<endl;
+			cout<<"5) complemento"<<endl;
+			cout<<"6) cadena"<<endl;
+			cout<<"7) union"<<endl;
+			cout<<"8) reverso(plus)"<<endl;
+			cout<<"0) salir"<<endl;
+			cin>>n;
+			if (n==0)
+			{
+				break;
+			}
+			if (n==1)
+			{
+				cout<<"como lo deseas guardar?"<<endl;
+				string ss;
+				cin>>ss;
+				g.grabar(ss);
+			}
+			if (n==4)
+			{
+				g.expor();
+				system("display img.png");
+			}
+			if (n==2)
+			{
+				grafo gg(0);
+				gg=g.determinista();
+				bool bb=false;
+				for (int i = 0; i < g.aristas.size(); ++i)
+				{
+					for (int j = 0; j < g.aristas.size(); ++j)
+					{
+						for (int k = 0; k < g.aristas[i][j].valor.size(); ++k)
+						{
+							if (g.aristas[i][j].valor[k]!=gg.aristas[i][j].valor[k])
+							{
+								b=true;
+							}
+						}
+					}
+				}
+				// if (equal(g.nodos.begin(),g.nodos.end(),gg.nodos.begin()) && equal(g.aristas.begin(), g.aristas.end(), gg.aristas.begin()) && equal(g.alfabeto.begin(),g.alfabeto.end(), gg.alfabeto.begin()) && equal(g.inicial.begin(),g.inicial.end(),gg.inicial.begin()) && equal(g.final.begin(),g.final.end(),gg.final.begin()))
+				if(bb)
+				{
+					cout<<"ERROR!, ya es determinista"<<endl;
+				}
+				else{
+					g=gg;
+				}
+			}
+			if (n==6)
+			{
+				cout<<"ingrese cadena: ";
+				string s;
+				cin>>s;
+				if (g.acepta(s))
+				{
+					cout<<"yes!"<<endl;
+				}
+				else{
+					cout<<"no!"<<endl;
+				}
+			}
+			if (n==3)
+			{
+				g=g.determinista();
+				g=minimizacion(g);
+			}
+			if (n==5)
+			{
+				g.complemento();
+			}
+			if (n==7)
+			{
+				string ss;
+				grafo gg(0);
+				cout<<"cual desea unir?"<<endl;
+				cin>>ss;
+				gg.cargar(ss);
+				g=nion(g,gg);
+			}
+			if (n==8)
+			{
+				g=g.reverso();
 			}
 		}
 	}
